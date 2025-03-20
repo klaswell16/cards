57d49601dc49be06b6a2c2f36ba8c3ec18eb473d
Kayan Laswell <kcclaswell@gmail.com>
2025-03-20 15:46:17 -0400

    initial comit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    hi
+
+    
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+
+
+
+</style>
